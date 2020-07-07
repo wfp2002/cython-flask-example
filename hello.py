6e7b4922fe5c8c9e802f5d466458b84c0e1c9bba
Wagner Fabiano <wfp2002@msn.com>
2020-07-07 17:06:55 -0300

    Sample

---
+++ hello.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+#acessar /greet?name=wfp
+@app.route('/greet')
+def greet():
+    name = request.args['name']
+    return '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Hi {}</h1>
+    </body>
+    </html>'''.format(escape(name))
+
+
+app.run()
